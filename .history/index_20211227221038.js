be3512a0f2973580f2e594ac1f5bab1fde49022b
Noammm8 <noammm8@gmail.com>
2021-12-27 23:33:04 +0200

    project#2

---
+++ .history/index_20211227221038.js
@@ -0,0 +1,24 @@
+const hourEl = document.getElementById("hours");
+const minuteEl = document.getElementById("minutes");
+const secondEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
+
+function updateClock(){
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+
+    if (h > 12){
+        h = h -12;
+        ampm = "PM";
+    }
+
+    hourEl.innerText = 8;
+    minuteEl.innerText = m;
+    secondEl.innerText = s;
+    ampmEl.innerText = ampm;
+
+}
+
+updateClock();
